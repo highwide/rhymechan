64e466629202439baef330dabfec8f0a8ef15e8a
Hirofumi Wakasugi <baenej@gmail.com>
2016-09-14 10:46:16 +0900

    Add feature to set created_by to a message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -51,6 +51,6 @@ class MessagesController < ApplicationController
     end
 
     def message_params
-      params.require(:message).permit(:content)
+      params.require(:message).permit(*%i(content created_by))
     end
 end
