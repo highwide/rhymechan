8ed2740ffe3deea0548199315fe9d308ea7cebb4
ta1kt0me <p.wadachi@gmail.com>
2016-10-06 21:17:49 +0900

    Remove non exist action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  before_action :set_message, only: [:show, :edit, :update, :destroy]
+  before_action :set_message, only: [:edit, :update, :destroy]
 
   def index
     @messages = Message.all
